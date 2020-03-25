1fe067255fa94a3a325bc3cfcecc921c5936509c
h4rar <anton.h4rar@gmail.com>
2020-03-25 21:45:16 +0700

    Теперь пользователь сам вводит время

--- app/src/main/java/h4rar/space/stopwatch/TimerActivity.java
+++ app/src/main/java/h4rar/space/stopwatch/TimerActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,29 +12,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Locale;
 
 public class TimerActivity extends AppCompatActivity {
-    private static final long START_TIME_IN_MILLIS = 600000;
-    private TextView textViewCountDown;
-    private Button buttonStartPause;
-    private Button buttonReset;
-
-    private CountDownTimer countDownTimer;
-
+    private long startTimeSecond;
+    private long startTimeMinutes;
     private boolean timerRunning;
-
-    private long timeInMillis = START_TIME_IN_MILLIS;
+    private long timeInMillis;
     private long endTime;
 
+    private Button buttonStartPause;
+    private Button buttonReset;
+    private EditText seconds;
+    private EditText minutes;
+
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
-        textViewCountDown = findViewById(R.id.timer_text__time);
-
+        seconds = findViewById(R.id.timer_edittext__seconds);
+        minutes = findViewById(R.id.timer_edittext__minutes);
         buttonStartPause = findViewById(R.id.timer_button__start_pause);
         buttonReset = findViewById(R.id.timer_button__reset);
-
+        setEditTextZero();
         buttonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,38 +45,45 @@ public class TimerActivity extends AppCompatActivity {
                 }
             }
         });
-
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resetTimer();
             }
         });
-
-        updateCountDownText();
     }
 
     private void startTimer() {
-        //чтобы при перевороте время не "терялось" сначала
-        //добавляю к currentTimeMillis текущее время телефона, азатем вычитаю
-        endTime = System.currentTimeMillis() + timeInMillis;
 
-        countDownTimer = new CountDownTimer(timeInMillis, 1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-                timeInMillis = millisUntilFinished;
-                updateCountDownText();
-            }
+        if (seconds.length() > 0 && minutes.length() > 0) {
+            startTimeSecond = Long.parseLong(seconds.getText().toString());
+            startTimeMinutes = Long.parseLong(minutes.getText().toString());
 
-            @Override
-            public void onFinish() {
-                timerRunning = false;
+            if (startTimeSecond != 0 || startTimeMinutes != 0) {
+                timeInMillis = startTimeSecond * 1000 + startTimeMinutes * 60000;
+
+                //чтобы при перевороте время не "терялось" сначала
+                //добавляю к currentTimeMillis текущее время телефона, азатем вычитаю
+                endTime = System.currentTimeMillis() + timeInMillis;
+
+                countDownTimer = new CountDownTimer(timeInMillis, 1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        timeInMillis = millisUntilFinished;
+                        updateCountDownText();
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        timerRunning = false;
+                        updateButtons();
+                    }
+                }.start();
+
+                timerRunning = true;
                 updateButtons();
             }
-        }.start();
-
-        timerRunning = true;
-        updateButtons();
+        }
     }
 
     private void pauseTimer() {
@@ -86,16 +93,18 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     private void resetTimer() {
-        timeInMillis = START_TIME_IN_MILLIS;
-        updateCountDownText();
+        setEditTextZero();
         updateButtons();
+        buttonReset.setVisibility(View.INVISIBLE);
     }
 
     private void updateCountDownText() {
-        int minutes = (int) (timeInMillis / 1000) / 60;
-        int seconds = (int) (timeInMillis / 1000) % 60;
-        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
-        textViewCountDown.setText(timeFormatted);
+        int minutes_time = (int) (timeInMillis / 1000) / 60;
+        int seconds_time = (int) (timeInMillis / 1000) % 60;
+        String minutesTimeFormatted = String.format(Locale.getDefault(), "%02d", minutes_time);
+        String secondsTimeFormatted = String.format(Locale.getDefault(), "%02d", seconds_time);
+        seconds.setText(secondsTimeFormatted);
+        minutes.setText(minutesTimeFormatted);
     }
 
     private void updateButtons() {
@@ -104,18 +113,21 @@ public class TimerActivity extends AppCompatActivity {
             buttonReset.setVisibility(View.INVISIBLE);
         } else {
             buttonStartPause.setText(R.string.start);
-            if (timeInMillis < 1000) {
-                buttonStartPause.setVisibility(View.INVISIBLE);
-            } else {
-                buttonStartPause.setVisibility(View.VISIBLE);
-            }
-            if (timeInMillis < START_TIME_IN_MILLIS) {
+            buttonStartPause.setVisibility(View.VISIBLE);
+
+            if (timeInMillis > 1000) {
                 buttonReset.setVisibility(View.VISIBLE);
             } else {
                 buttonReset.setVisibility(View.INVISIBLE);
             }
-
         }
+    }
+
+    private void setEditTextZero() {
+        String minutesTimeFormatted = String.format(Locale.getDefault(), "%02d", 0);
+        String secondsTimeFormatted = String.format(Locale.getDefault(), "%02d", 0);
+        seconds.setText(secondsTimeFormatted);
+        minutes.setText(minutesTimeFormatted);
     }
 
     @Override
@@ -132,7 +144,6 @@ public class TimerActivity extends AppCompatActivity {
 
         timeInMillis = savedInstanceState.getLong("millis");
         timerRunning = savedInstanceState.getBoolean("timerRunning");
-        updateCountDownText();
         updateButtons();
 
         if (timerRunning) {
